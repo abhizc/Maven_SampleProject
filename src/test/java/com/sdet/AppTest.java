02c680093127b60b4cb73feeeff579a4708bd720
C <abhishek.c@capgemini.com>
2022-05-24 10:55:11 +0530

    Initial Commit

---
+++ src/test/java/com/sdet/AppTest.java
@@ -0,0 +1,20 @@
+package com.sdet;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+    @Test
+    public void testlogin1(){
+        App myapp = new App();
+        Assert.assertEquals(0,myapp.userLogin("abc","abcd"));
+    }
+
+    @Test
+    public void testlogin2(){
+        App myapp = new App();
+        Assert.assertEquals(1,myapp.userLogin("abc","123"));
+    }
+
+}
